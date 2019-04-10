e7f821f5f3f57c08ee05a7abab2d47e7ecb57702
wesley-hall <hallwesley@outlook.com>
2019-04-10 11:56:03 +0100

    messages added to routes

--- config/routes.py
+++ config/routes.py
@@ -1,7 +1,8 @@
 from app import app
-from controllers import auth, posts, profiles, external
+from controllers import auth, posts, profiles, messages, external
 
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(posts.api, url_prefix='/api')
 app.register_blueprint(profiles.api, url_prefix='/api')
+app.register_blueprint(messages.api, url_prefix='/api')
 app.register_blueprint(external.api, url_prefix='/api')
