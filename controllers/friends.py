bec8b936b356497dba1344b517db024cdf0eea98
wesley-hall <hallwesley@outlook.com>
2019-04-10 16:52:40 +0100

    add friend working correctly

--- controllers/friends.py
+++ controllers/friends.py
@@ -18,27 +18,25 @@ def friend_pending(friend_a, friend_b):
 @secure_route
 def add_friend():
     data = request.get_json()
-    friend, errors = friend_schema.load(data)
-    print(data, 'THISISTHEDATA')
-    print(data['friend_b_id'], 'THISISFRIENDID')
-    print(friend)
-    friend.friend_b = User.query.filter_by(id=data['friend_b_id']).first()
-    print('B', friend.friend_b)
+    friend_request, errors = friend_schema.load(data)
+
     if errors:
         return jsonify(errors), 422
-        friend.friend_a = g.current_user
-    print('A', friend.friend_a)
 
-    is_friend, is_pending = friend_pending(friend.friend_a, friend.friend_b)
+    requester = g.current_user
+    requestee = User.query.get(data['friend_b_id'])
 
-    if friend.friend_a == friend.friend_b:
-        return jsonify({'message': 'You cannot add yourself as a friend'})
+    is_friend, is_pending = friend_pending(requester.id, requestee.id)
+
+    if requester.id == requestee.id:
+        return jsonify({'message': 'You cannot add yourself as a friend'}), 422
     if is_friend:
-        return jsonify({'message': 'You are already friend'})
+        return jsonify({'message': 'You are already friends'}), 422
     if is_pending:
-        return jsonify({'message': 'Your friend request is pending'})
-    friend = Friend(friend_a_id=friend.friend_a,
-    friend_b_id=friend.friend_b, status='Requested')
+        return jsonify({'message': 'Your friend request is pending'}), 422
 
-    friend.save()
-    return friend_schema.jsonify(friend), 201
+    friend_request = Friend(friend_a_id=requester.id,
+    friend_b_id=requestee.id, status='Requested')
+
+    friend_request.save()
+    return friend_schema.jsonify(friend_request), 201
