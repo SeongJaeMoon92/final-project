e07197bc006b84dd996e3ea8cc8abd0c548a6554
wesley-hall <hallwesley@outlook.com>
2019-04-11 12:05:51 +0100

    education details updated

--- src/components/page/profileEducation.js
+++ src/components/page/profileEducation.js
@@ -5,7 +5,10 @@ const ProfileEducation = (props) => {
   return(
     <div>
       <h4>{education.school}</h4>
-      <h5>{education.degree}</h5>
+      <h5>{education.degree}{education.grade && `, ${education.grade}`}</h5>
+      <h5>{education.field_of_study}</h5>
+      <h5>{education.date_from === education.date_to ? education.date_from : `${education.date_from} - ${education.date_to}`}</h5>
+      {education.description && <p>{education.description}</p>}
     </div>
   )
 
