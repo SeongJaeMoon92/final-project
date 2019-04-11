aa224d373df2841c3422c17c10b0459ae1d12327
wesley-hall <hallwesley@outlook.com>
2019-04-11 12:06:04 +0100

    experience details updated

--- src/components/page/profileExperience.js
+++ src/components/page/profileExperience.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const ProfileExperience = (props) => {
-  const { } = props
+  const { experience } = props
   return(
     <div>
-      xxx
+      <h4>{experience.company}</h4>
+      <h5>{experience.title}</h5>
+      <h5>{experience.location}</h5>
+      <h5>{experience.start_date === experience.end_date ? experience.start_date : `${experience.start_date} - ${experience.end_date}`}</h5>
+      {experience.description && <p>{experience.description}</p>}
     </div>
   )
 
