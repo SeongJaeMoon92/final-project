be85d125d3baadf46dfcea5ee9149a7890541cc4
wesley-hall <hallwesley@outlook.com>
2019-04-15 00:09:21 +0100

    networking functionality for each profile

--- src/components/page/network/networkProfile.js
+++ src/components/page/network/networkProfile.js
@@ -2,21 +2,49 @@ import React from 'react'
 import { Card, Image, Button, ButtonToolbar } from 'react-bootstrap'
 
 const NetworkProfile = (props) => {
-  const { profile } = props
+  const { profile, ownerId, isConnection, isPendingReceived, isPendingSent, requestConnection, approveConnection } = props
+  console.log('isConnection', profile.id, isConnection(profile))
+  console.log('isPendingReceived', profile.id, isPendingReceived(profile))
+  console.log('isPendingSent', profile.id, isPendingSent(profile))
   return(
     <Card className="text-center">
       <Card.Body>
         {<Image src={profile.image ? profile.image : '../../../assets/images/profiles/no_image.jpg'} roundedCircle />}
         <Card.Title>{profile.name}</Card.Title>
-        <Card.Text>
-          {profile.headline} - {profile.location}
-        </Card.Text>
+        <Card.Text>{profile.headline}</Card.Text>
+        {isConnection(profile) && <Card.Text>JoinedUp</Card.Text>}
       </Card.Body>
       <Card.Footer>
         <ButtonToolbar>
-          <Button variant="primary" href={`/profile/${profile.id}`}>View Profile</Button>
-          <Button variant="primary" href={`/profile/${profile.id}`}>Request Connection</Button>
-          <Button variant="primary" href={`/profile/${profile.id}`} disabled>Connection Requested</Button>
+          <Button
+            variant="primary"
+            href={`/profile/${profile.id}`}
+          >
+            View Profile
+          </Button>
+          {!isConnection(profile) &&
+            !isPendingReceived(profile) &&
+            !isPendingSent(profile) &&
+            <Button
+              variant="primary"
+              value={ownerId}
+              onClick={requestConnection}
+            >
+              Request Connection
+            </Button>
+          }
+          {isPendingReceived(profile) &&
+            <Button
+              variant="primary"
+              value={isPendingReceived(profile).id}
+              onClick={approveConnection}
+            >
+              Approve Connection Request
+            </Button>
+          }
+          {isPendingSent(profile) &&
+            <p>Connection Requested</p>
+          }
         </ButtonToolbar>
       </Card.Footer>
     </Card>
