6df241032a2716125e160c681fecad0659a23a25
wesley-hall <hallwesley@outlook.com>
2019-04-16 15:08:57 +0100

    layout updated

--- src/components/common/profileRequired.js
+++ src/components/common/profileRequired.js
@@ -3,15 +3,22 @@ import { Container, Row, Button } from 'react-bootstrap'
 
 const ProfileRequired = () => {
   return(
-    <Container className="container-fluid my-3 container-min-height">
-      <Row className="justify-content-center text-center mt-5 mb-2">
-        <h4>You need to create a profile before being able to access this page.</h4>
-      </Row>
-      <Row className="justify-content-center m-4">
-        <Button variant="primary" href="/profile/new">Create a profile</Button>
-      </Row>
+    <Container className="my-3 container-min-height">
+      <div className="h-100">
+        <Row className="justify-content-center text-center mt-5 mb-2">
+          <h4>You need to create a profile before being able to access this page.</h4>
+        </Row>
+        <Row className="justify-content-center m-4">
+          <Button variant="primary" href="/profile/new">Create a profile</Button>
+        </Row>
+      </div>
     </Container>
   )
 }
 
 export default ProfileRequired
+// if (!userProfile) return <ProfileRequired/>
+//
+//
+//
+// import ProfileRequired from '../../common/profileRequired'
