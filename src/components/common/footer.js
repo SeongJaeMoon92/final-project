d03c9805ecf0a8b1b88e42d47a6450b1616177b7
wesley-hall <hallwesley@outlook.com>
2019-04-16 15:08:14 +0100

    footer size, colour adjusted to match header

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -5,13 +5,13 @@ const Footer = () => {
 
   return(
 
-    <Navbar collapseOnSelect sticky="bottom" expand="lg" bg="primary" variant="dark" className="justify-content-center">
+    <Navbar collapseOnSelect sticky="bottom" expand="lg" bg="secondary" variant="dark" className="justify-content-center">
       <Nav>
-        <Nav.Link href="/">
+        <Nav.Link className="p-1" href="/">
           <img
             alt="JoinedUp"
             src="../../assets/images/joinedup/joinedup_logo_white_small.png"
-            height="30"
+            height="32"
             className="d-inline-block align-top"
           />
         </Nav.Link>
